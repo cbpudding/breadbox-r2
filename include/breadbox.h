b4aa566d6ddce21ac0ca66fc92d28b8f7748a424
Alexander Hill <ahill@breadpudding.dev>
2023-04-18 20:02:10 -0400

    Cleaned the breadbox_init function up

--- include/breadbox.h
+++ include/breadbox.h
@@ -3,6 +3,11 @@
 
 #define BREADBOX_PORT 1491
 
+#define BREADBOX_MODULE_MODEL 1 << 0
+#define BREADBOX_MODULE_VIEW 1 << 1
+#define BREADBOX_MODULE_UPDATE 1 << 2
+#define BREADBOX_MODULE_SUBSCRIPTION 1 << 3
+
 #include "log.h"
 #include "message.h"
 #include "model.h"
@@ -12,11 +17,12 @@
 #include "view.h"
 
 typedef struct {
-	breadbox_model_t model;
+	breadbox_model_t *model;
+	int modules;
 	breadbox_options_t *options;
-	breadbox_subs_t subs;
-	breadbox_update_t update;
-	breadbox_view_t view;
+	breadbox_subs_t *subs;
+	breadbox_update_t *update;
+	breadbox_view_t *view;
 } breadbox_t;
 
 int breadbox_init(breadbox_t *engine, breadbox_options_t *options);
--- src/breadbox.c
+++ src/breadbox.c
@@ -5,84 +5,145 @@
 #include "breadbox.h"
 
 int breadbox_init(breadbox_t *engine, breadbox_options_t *options) {
+	engine->model = NULL;
+	engine->modules = 0;
 	engine->options = options;
-	if(!SDL_Init(SDL_INIT_EVERYTHING)) {
-		if(PHYSFS_init(options->meta)) {
-			if(!breadbox_model_init(&engine->model)) {
-				if(!breadbox_update_init(&engine->update)) {
-					if(!breadbox_subs_init(&engine->subs)) {
-						if(!breadbox_view_init(
-							   &engine->view, engine->options
-						   )) {
-							breadbox_log_info(
-								BBLOG_BREADBOX,
-								"Successfully initialized the engine in %dms",
-								SDL_GetTicks()
-							);
-							return 0;
-						} else {
-							// I complain about non-specific error messages all
-							// the time at my job so why am I doing it here?!?
-							// ~Alex
-							breadbox_log_error(
-								BBLOG_VIEW, "Failed to initialize view"
-							);
-						}
-						breadbox_subs_fini(&engine->subs);
-					} else {
-						// Yet another comment about myself complaining to
-						// myself about non-specific error messages. ~Alex
-						breadbox_log_error(
-							BBLOG_SUBSCRIPTION,
-							"Failed to initialize subscriptions"
-						);
-					}
-					breadbox_update_fini(&engine->update);
-				} else {
-					// There's probably a better way of handling this. We should
-					// at least say why we can't. ~Alex
-					breadbox_log_error(
-						BBLOG_UPDATE, "Failed to initialize update"
-					);
-				}
-				breadbox_model_fini(&engine->model);
-			} else {
-				// Okay, but why? ~Alex
-				breadbox_log_error(
-					BBLOG_MODEL, "Failed to initialize the model"
-				);
-			}
-			PHYSFS_deinit();
-		} else {
-			breadbox_log_error(
-				BBLOG_BREADBOX,
-				"Failed to initialize PhysFS: %s",
-				PHYSFS_getLastError()
-			);
-		}
-		SDL_Quit();
-	} else {
+	engine->subs = NULL;
+	engine->update = NULL;
+	engine->view = NULL;
+	if(SDL_Init(SDL_INIT_EVERYTHING)) {
 		breadbox_log_error(
 			BBLOG_BREADBOX, "Failed to initialize SDL2: %s", SDL_GetError()
 		);
+		breadbox_fini(engine);
+		return 1;
 	}
-	return 1;
+	if(!PHYSFS_init(options->meta)) {
+		breadbox_log_error(
+			BBLOG_BREADBOX,
+			"Failed to initialize PhysFS: %s",
+			PHYSFS_getLastError()
+		);
+		breadbox_fini(engine);
+		return 1;
+	}
+	engine->model = malloc(sizeof(breadbox_model_t));
+	if(!engine->model) {
+		breadbox_log_error(
+			BBLOG_MODEL, "Not enough memory to allocate the model"
+		);
+		breadbox_fini(engine);
+		return 1;
+	}
+	if(breadbox_model_init(engine->model)) {
+		// Okay, but why? ~Alex
+		breadbox_log_error(BBLOG_MODEL, "Failed to initialize the model");
+		breadbox_fini(engine);
+		return 1;
+	}
+	engine->modules |= BREADBOX_MODULE_MODEL;
+	engine->update = malloc(sizeof(breadbox_update_t));
+	if(!engine->update) {
+		breadbox_log_error(
+			BBLOG_UPDATE, "Not enough memory to allocate update module"
+		);
+		breadbox_fini(engine);
+		return 1;
+	}
+	if(breadbox_update_init(engine->update)) {
+		// There's probably a better way of handling this. We should
+		// at least say why we can't. ~Alex
+		breadbox_log_error(
+			BBLOG_UPDATE, "Failed to initialize the update module"
+		);
+		breadbox_fini(engine);
+		return 1;
+	}
+	engine->modules |= BREADBOX_MODULE_UPDATE;
+	engine->subs = malloc(sizeof(breadbox_subs_t));
+	if(!engine->subs) {
+		breadbox_log_error(
+			BBLOG_SUBSCRIPTION,
+			"Not enough memory to allocate the subscription handler"
+		);
+		breadbox_fini(engine);
+		return 1;
+	}
+	if(breadbox_subs_init(engine->subs)) {
+		// Yet another comment about myself complaining to
+		// myself about non-specific error messages. ~Alex
+		breadbox_log_error(
+			BBLOG_SUBSCRIPTION, "Failed to initialize the subscription handler"
+		);
+		breadbox_fini(engine);
+		return 1;
+	}
+	engine->modules |= BREADBOX_MODULE_SUBSCRIPTION;
+	engine->view = malloc(sizeof(breadbox_view_t));
+	if(!engine->view) {
+		breadbox_log_error(
+			BBLOG_VIEW, "Not enough memory to allocate the view function"
+		);
+		breadbox_fini(engine);
+		return 1;
+	}
+	if(breadbox_view_init(engine->view, engine->options)) {
+		// I complain about non-specific error messages all
+		// the time at my job so why am I doing it here?!?
+		// ~Alex
+		breadbox_log_error(
+			BBLOG_VIEW, "Failed to initialize the view function"
+		);
+		breadbox_fini(engine);
+		return 1;
+	}
+	engine->modules |= BREADBOX_MODULE_VIEW;
+	breadbox_log_info(
+		BBLOG_BREADBOX,
+		"Successfully initialized the engine in %dms",
+		SDL_GetTicks()
+	);
+	return 0;
 }
 
 void breadbox_fini(breadbox_t *engine) {
-	breadbox_view_fini(&engine->view);
-	breadbox_subs_fini(&engine->subs);
-	breadbox_update_init(&engine->update);
-	breadbox_model_fini(&engine->model);
-	PHYSFS_deinit();
-	SDL_Quit();
+	if(engine->modules & BREADBOX_MODULE_VIEW) {
+		breadbox_view_fini(engine->view);
+	}
+	if(engine->view) {
+		free(engine->view);
+	}
+	if(engine->modules & BREADBOX_MODULE_SUBSCRIPTION) {
+		breadbox_subs_fini(engine->subs);
+	}
+	if(engine->subs) {
+		free(engine->subs);
+	}
+	if(engine->modules & BREADBOX_MODULE_UPDATE) {
+		breadbox_update_fini(engine->update);
+	}
+	if(engine->update) {
+		free(engine->update);
+	}
+	if(engine->modules & BREADBOX_MODULE_MODEL) {
+		breadbox_model_fini(engine->model);
+	}
+	if(engine->model) {
+		free(engine->model);
+	}
+	if(PHYSFS_isInit()) {
+		PHYSFS_deinit();
+	}
+	if(SDL_WasInit(0)) {
+		SDL_Quit();
+	}
 }
 
 void breadbox_run(breadbox_t *engine) {
 	bool alive = true;
 	breadbox_msg_t msg;
 	while(alive) {
-		if(breadbox_subs_poll(&engine->subs, engine->options, &msg)) {
+		if(breadbox_subs_poll(engine->subs, engine->options, &msg)) {
 			switch(msg) {
 				case BBMSG_QUIT:
 					breadbox_log_info(
@@ -92,10 +153,10 @@ void breadbox_run(breadbox_t *engine) {
 					alive = false;
 					break;
 				case BBMSG_VIEW:
-					breadbox_view_render(&engine->view, &engine->model);
+					breadbox_view_render(engine->view, engine->model);
 					break;
 				default:
-					breadbox_update_msg(&engine->update, &engine->model, &msg);
+					breadbox_update_msg(engine->update, engine->model, &msg);
 					break;
 			}
 		}
