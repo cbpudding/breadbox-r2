06815dac3fdab3b0c962a81f425fa2a082cb46e1
Alexander Hill <ahill@breadpudding.dev>
2023-04-19 23:04:14 -0400

    Matrix and vector math time

--- include/view.h
+++ include/view.h
@@ -6,6 +6,38 @@
 #ifndef VIEW_H
 #define VIEW_H
 
+// Technically, this could be float[16] but I'm using letters to preserve what
+// little remains of my sanity. ~Alex
+typedef struct {
+	float a;
+	float b;
+	float c;
+	float d;
+	float e;
+	float f;
+	float g;
+	float h;
+	float i;
+	float j;
+	float k;
+	float l;
+	float m;
+	float n;
+	float o;
+	float p;
+} breadbox_matrix_t;
+
+typedef struct {
+	float x;
+	float y;
+	float z;
+	float w;
+} breadbox_vector_t;
+
+void mult_matrix_matrix(breadbox_matrix_t *product, breadbox_matrix_t *f1, breadbox_matrix_t *f2);
+void mult_matrix_vector(breadbox_vector_t *product, breadbox_matrix_t *f1, breadbox_vector_t *f2);
+void mult_vector_vector(breadbox_vector_t *product, breadbox_vector_t *f1, breadbox_vector_t *f2);
+
 typedef struct {
 	SDL_GLContext context;
 	SDL_Window *window;
--- src/view.c
+++ src/view.c
@@ -11,6 +11,40 @@
 #include "log.h"
 #include "view.h"
 
+// I hope I did all of this correctly... ~Alex
+void mult_matrix_matrix(breadbox_matrix_t *product, breadbox_matrix_t *f1, breadbox_matrix_t *f2) {
+	product->a = (f1->a * f2->a) + (f1->e * f2->b) + (f1->i * f2->c) + (f1->m * f2->d);
+	product->b = (f1->b * f2->a) + (f1->f * f2->b) + (f1->j * f2->c) + (f1->n * f2->d);
+	product->c = (f1->c * f2->a) + (f1->g * f2->b) + (f1->k * f2->c) + (f1->o * f2->d);
+	product->d = (f1->d * f2->a) + (f1->h * f2->b) + (f1->l * f2->c) + (f1->p * f2->d);
+	product->e = (f1->a * f2->e) + (f1->e * f2->f) + (f1->i * f2->g) + (f1->m * f2->h);
+	product->f = (f1->b * f2->e) + (f1->f * f2->f) + (f1->j * f2->g) + (f1->n * f2->h);
+	product->g = (f1->c * f2->e) + (f1->g * f2->f) + (f1->k * f2->g) + (f1->o * f2->h);
+	product->h = (f1->d * f2->e) + (f1->h * f2->f) + (f1->l * f2->g) + (f1->p * f2->h);
+	product->i = (f1->a * f2->i) + (f1->e * f2->j) + (f1->i * f2->k) + (f1->m * f2->l);
+	product->j = (f1->b * f2->i) + (f1->f * f2->j) + (f1->j * f2->k) + (f1->n * f2->l);
+	product->k = (f1->c * f2->i) + (f1->g * f2->j) + (f1->k * f2->k) + (f1->o * f2->l);
+	product->l = (f1->d * f2->i) + (f1->h * f2->j) + (f1->l * f2->k) + (f1->p * f2->l);
+	product->m = (f1->a * f2->m) + (f1->e * f2->n) + (f1->i * f2->o) + (f1->m * f2->p);
+	product->n = (f1->b * f2->m) + (f1->f * f2->n) + (f1->j * f2->o) + (f1->n * f2->p);
+	product->o = (f1->c * f2->m) + (f1->g * f2->n) + (f1->k * f2->o) + (f1->o * f2->p);
+	product->p = (f1->d * f2->m) + (f1->h * f2->n) + (f1->l * f2->o) + (f1->p * f2->p);
+}
+
+void mult_matrix_vector(breadbox_vector_t *product, breadbox_matrix_t *f1, breadbox_vector_t *f2) {
+	product->x = (f1->a * f2->x) + (f1->b * f2->y) + (f1->c * f2->z) + (f1->d * f2->w);
+	product->y = (f1->e * f2->x) + (f1->f * f2->y) + (f1->g * f2->z) + (f1->h * f2->w);
+	product->z = (f1->i * f2->x) + (f1->j * f2->y) + (f1->k * f2->z) + (f1->l * f2->w);
+	product->w = (f1->m * f2->x) + (f1->n * f2->y) + (f1->o * f2->z) + (f1->p * f2->w);
+}
+
+void mult_vector_vector(breadbox_vector_t *product, breadbox_vector_t *f1, breadbox_vector_t *f2) {
+	product->x = f1->x * f2->x;
+	product->y = f1->y * f2->y;
+	product->z = f1->z * f2->z;
+	product->w = f1->w * f2->w;
+}
+
 int breadbox_view_init(breadbox_view_t *view, breadbox_options_t *options) {
 	// ATI Rage or bust! ~Alex
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 1);
